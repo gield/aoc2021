e9374f0220c2a40e0b4e420570b1695cf45d2c19
Giel Dops <gieldops@hotmail.com>
2021-12-02 09:06:21 +0100

    Use Python's new fancy pattern matching

--- day02/solution1.py
+++ day02/solution1.py
@@ -1,13 +1,13 @@
 with open("input.txt", "r") as f:
     lines = [l.strip() for l in f.readlines()]
 
-horizontal, depth = 0, 0
+horizontal = depth = 0
 for line in lines:
-    instruction, num = line.split()
-    if instruction == "forward":
-        horizontal += int(num)
-    elif instruction == "down":
-        depth += int(num)
-    elif instruction == "up":
-        depth -= int(num)
+    match line.split():
+        case "forward", num:
+            horizontal += int(num)
+        case "down", num:
+            depth += int(num)
+        case "up", num:
+            depth -= int(num)
 print(horizontal * depth)
--- day02/solution2.py
+++ day02/solution2.py
@@ -1,14 +1,14 @@
 with open("input.txt", "r") as f:
     lines = [l.strip() for l in f.readlines()]
 
-horizontal, depth, aim = 0, 0, 0
+horizontal = depth = aim = 0
 for line in lines:
-    instruction, num = line.split()
-    if instruction == "forward":
-        horizontal += int(num)
-        depth += aim * int(num)
-    elif instruction == "down":
-        aim += int(num)
-    elif instruction == "up":
-        aim -= int(num)
+    match line.split():
+        case "forward", num:
+            horizontal += int(num)
+            depth += aim * int(num)
+        case "down", num:
+            aim += int(num)
+        case "up", num:
+            aim -= int(num)
 print(horizontal * depth)
