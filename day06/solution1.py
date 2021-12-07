8d6054ae69d1c95d4320a080e72c8cf850e3c621
Giel Dops <gieldops@hotmail.com>
2021-12-07 02:10:45 +0100

    Add day 6

---
+++ day06/solution1.py
@@ -0,0 +1,13 @@
+with open("input.txt", "r") as f:
+    line = f.readline().strip()
+
+population = {i: 0 for i in range(9)}
+for i in line.split(","):
+    population[int(i)] += 1
+for _ in range(80):
+    num_new_fish = population[0]
+    for i in range(8):
+        population[i] = population[i + 1]
+    population[6] += num_new_fish
+    population[8] = num_new_fish
+print(sum(population.values()))
